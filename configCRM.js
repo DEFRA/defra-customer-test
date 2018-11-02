53babf4220328cdae9c36c283dde7d08222c9f21
prateekshirdhonkar <prateek.shirdhonkar.ea@gmail.com>
2018-11-02 14:32:03 +0000

    Initial commit

---
+++ configCRM.js
@@ -0,0 +1,13 @@
+module.exports.configCRM = {
+  appUrlCRM: 'https://defra-custmstr-test.crm4.dynamics.com/',
+  userid01: 'prateek.shir@defradev.onmicrosoft.com',
+  pass01: 'Meherbaba2510$',
+  authorityURL: 'https://login.microsoftonline.com/common/oauth2/token',
+  resource: 'https://defra-custmstr-dev.crm4.dynamics.com',
+  clientID: 'f0ce8ded-50ec-4edc-8afe-c5b66157f963',
+  postmantoken: 'ec6fe982-31e1-4bc2-1f47-',
+  authorityHostUrl: 'https://login.microsoftonline.com',
+  tenant: 'defradev.onmicrosoft.com',
+  clientSecret: 'lUvlF1kKTQhuB4mgo5td1AxaRCPHDLF5zoRk1eauZ5U=',
+  loadChromeNmpExtension: false
+}
