48dbd8db2b84269cbc54705d3638ae19d97ece49
Robert Bartley <robert.bartley@defra.gov.uk>
2019-11-21 16:54:08 +0000

    added tokenised crm config

---
+++ configCRM.js
@@ -0,0 +1,11 @@
+module.exports.configCRM = {
+  appUrlCRM: '#{appUrlCRM}#',
+  authorityURL: '#{authorityURL}#',
+  resource: '#{resource}#',
+  clientID: '#{clientID}#',
+  postmantoken: '#{postmantoken}#',
+  authorityHostUrl: '#{authorityHostUrl}#',
+  tenant: '#{tenant}#',
+  clientSecret: '#{clientSecret}#',
+  loadChromeNmpExtension: false
+}
