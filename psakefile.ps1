88d174d3c9b03ecf2e59238b1b91237891c9c3a4
Robert Bartley <robert.bartley@defra.gov.uk>
2019-11-28 15:44:40 +0000

    added install task

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,5 +1,10 @@
 task default -depends Test
 
-task Test -Description "Run node cucumber tests" {
+task Test -depends Install -Description  "Run node cucumber tests" {
     Exec {npm run test:chrome-api-test}
 }
+
+task Install -Description "Adds required node modules into bin folder" {
+    Exec {npm install}
+}
+
