04bbe780107d3472985f78bfcd9da78c343ed9e8
Robert Bartley <robert.bartley@defra.gov.uk>
2019-12-02 14:35:27 +0000

    added junit task

--- cucumberjs/systemTests/apiTests/support/hooks.js
+++ cucumberjs/systemTests/apiTests/support/hooks.js
@@ -8,50 +8,52 @@ let frontEndVersion;
 let backEndVersion;
 
 module.exports = function () {
-	
+
 	this.Before((scenario) => {
 		scenario.attach(getWorldParameters().platform);
 
 	});
 
-	// this.After(async () => {
-	// 	const getBrowserHandle = await driver.getBrowser();
-	// 	return driver.quitBrowser();
-
-	// });
 	this.registerHandler('ScenarioResult', async function (scenario) {
 		if (scenario.status === 'failed') {
-		//   const filename = await driver.browser.getCurrentUrl()
-		//   driver.takeScreenshotsAfterFailure(filename)
-		  return driver.quitBrowser()
+			//   const filename = await driver.browser.getCurrentUrl()
+			//   driver.takeScreenshotsAfterFailure(filename)
+			return driver.quitBrowser()
 		} else {
-		  return driver.quitBrowser()
+			return driver.quitBrowser()
 		}
-	  })
+	})
 
-		//ONLY ENABLE THIS IF YOU WANT REPORTS
-	 this.registerHandler("AfterFeatures", () => {
-	 	const reporter = require("cucumber-html-reporter");
+	this.registerHandler("AfterFeatures", () => {
 
-		const options = {
-	 		theme: "bootstrap",
-	 		jsonDir: "cucumberjs/systemTests/apiTests/reports",
-	 		output: "cucumberjs/systemTests/apiTests/reports/cucumber_report.html",
-	 		reportSuiteAsScenarios: true,
-	 		launchReport: true,
-	 		ignoreBadJsonFile: true,
-	 		metadata: {
-	 			"App Version": "Customer Management App",
-	 			"Test Environment": configCRM.appUrlCRM,
-	 			"Version": 0.1,
-	 			Platform: getWorldParameters().platform,
-	 			Platform: "Windows 10",
-	 			Parallel: "Scenarios",
-				Executed: "local"
-	 		}
-	 	};
-
-	 	reporter.generate(options);
-	 });
-
+		if (process.argv.includes("--junit")) {
+			const cucumberJunitConvert = require('cucumber-junit-convert');
+			const options = {
+				inputJsonFile: "reports/api-tests.json",
+				outputXmlFile: 'reports/cucumber_report_api.xml'
+			}
+			cucumberJunitConvert.convert(options);
+		}
+		else {
+			const reporter = require("cucumber-html-reporter");
+			const options = {
+				theme: "bootstrap",
+				jsonDir: "reports",
+				output: "reports/cucumber_report.html",
+				reportSuiteAsScenarios: true,
+				launchReport: true,
+				ignoreBadJsonFile: true,
+				metadata: {
+					"App Version": "Customer Management App",
+					"Test Environment": configCRM.appUrlCRM,
+					"Version": 0.1,
+					Platform: getWorldParameters().platform,
+					Platform: "Windows 10",
+					Parallel: "Scenarios",
+					Executed: "local"
+				}
+			};
+			reporter.generate(options);
+		}
+	});
 };
