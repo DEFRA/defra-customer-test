c0e46036addf055cc02046b4f768268888f647ea
Robert Bartley <robert.bartley@defra.gov.uk>
2019-12-02 16:53:56 +0000

    added create folder

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,6 +1,6 @@
 task default -depends Test
 
-task Test -depends Install -Description  "Run node cucumber tests" {
+task Test -depends Install, CreateOutputDir -Description  "Run node cucumber tests" {
     Exec {npm run test:chrome-api-test}
 }
 
@@ -8,3 +8,8 @@ task Install -Description "Adds required node modules into bin folder" {
     Exec {npm install}
 }
 
+task CreateOutputDir -Description "Adds required node modules into bin folder" {
+    if(!(Test-Path "./Reports/")){
+        New-Item "Reports\" -ItemType "directory"
+    }
+}
