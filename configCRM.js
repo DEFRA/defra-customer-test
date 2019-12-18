a29bc430565fda8c513c70fea1e5341ee65c2fd2
Robert Bartley <robert.bartley@defra.gov.uk>
2019-12-18 16:31:41 +0000

    added logging to show env url being used

--- configCRM.js
+++ configCRM.js
@@ -1,42 +1,43 @@
 const fs = require('fs')
 const dotenv = require('dotenv');
 const result = dotenv.config();
-if (result.error) {throw result.error;}
+if (result.error) { throw result.error; }
 const { parsed: config } = result;
 
-var crmConfig =  {}
- 
-if(config.RUN_LOCAL.toLowerCase() == "true"){
-  const envConfig =  dotenv.parse(fs.readFileSync(config.ENV_CONFIG))
+var crmConfig = {}
+
+if (config.RUN_LOCAL.toLowerCase() == "true") {
+  const envConfig = dotenv.parse(fs.readFileSync(config.ENV_CONFIG))
   if (envConfig.error) {
     throw envConfig.error;
   }
   console.log("loaded " + envConfig.NODE_ENV + " config")
-  crmConfig =  {
+  crmConfig = {
     appUrlCRM: envConfig.appUrlCRM,
     authorityURL: envConfig.authorityURL,
     resource: envConfig.resource,
     clientID: envConfig.clientID,
     postmantoken: envConfig.postmantoken,
-    authorityHostUrl: envConfig.authorityHostUrl ,
-    tenant: envConfig.tenant ,
-    clientSecret:  envConfig.clientSecret ,
+    authorityHostUrl: envConfig.authorityHostUrl,
+    tenant: envConfig.tenant,
+    clientSecret: envConfig.clientSecret,
     loadChromeNmpExtension: false
   }
 }
-else{
-  console.log("loaded tokenised config")
-  crmConfig =  {
-  appUrlCRM: '#{appUrlCRM}#',
-  authorityURL: '#{authorityURL}#',
-  resource: '#{resource}#',
-  clientID: '#{clientID}#',
-  postmantoken: '#{postmantoken}#',
-  authorityHostUrl: '#{authorityHostUrl}#',
-  tenant: '#{tenant}#',
-  clientSecret: '#{clientSecret}#',
-  loadChromeNmpExtension: false
+else {
+  crmConfig = {
+    appUrlCRM: '#{appUrlCRM}#',
+    authorityURL: '#{authorityURL}#',
+    resource: '#{resource}#',
+    clientID: '#{clientID}#',
+    postmantoken: '#{postmantoken}#',
+    authorityHostUrl: '#{authorityHostUrl}#',
+    tenant: '#{tenant}#',
+    clientSecret: '#{clientSecret}#',
+    loadChromeNmpExtension: false
   }
+
+  console.log("loaded tokenised config for " + crmConfig.appUrlCRM)
 }
 
 module.exports.configCRM = crmConfig
